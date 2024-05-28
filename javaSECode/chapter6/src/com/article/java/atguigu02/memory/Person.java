6851d7cc37276536e2dbb54fdaaafb8b7d1d4e77
Article <15124424349@163.com>
2024-05-28 16:56:09 +0800

    使用说明修正

--- javaSECode/chapter6/src/com/article/java/atguigu02/memory/Person.java
+++ javaSECode/chapter6/src/com/article/java/atguigu02/memory/Person.java
@@ -1,10 +1,33 @@
 package com.article.java.atguigu02.memory;
 
 /**
- * @author 尚硅谷-宋红康
- * @create 14:31
+ * @projectName: JavaProjectCode
+ * @package: com.article.java.Arrays
+ * @className: ArraySTest
+ * @author: 拾叁拾玖
+ * @description: TODO
+ * @date: 2024/5/16 上午9:58
+ * @version: 1.0
  */
 
 public class Person {
+    //属性
+    String name;//姓名
+    int age;//年龄
+    char gender;//性别
 
+
+    //方法
+    public void eat() {
+        System.out.println("吃饭");
+    }
+
+    public void sleep(int hour) {
+        System.out.println("人每天至少保持" + hour + "小时睡眠");
+    }
+
+    public void interests(String hobby) {
+        System.out.println("我的爱好是:" + hobby);
+    }
 }
+
--- javaSECode/chapter6/src/com/article/java/atguigu02/memory/PersonTest.java
+++ javaSECode/chapter6/src/com/article/java/atguigu02/memory/PersonTest.java
@@ -1,13 +1,35 @@
 package com.article.java.atguigu02.memory;
 
 /**
- * Perosn类对应的测试类
- *
- * @author 尚硅谷-宋红康
- * @create 14:38
+ * @projectName: JavaProjectCode
+ * @package: com.article.java.Arrays
+ * @className: ArraySTest
+ * @author: 拾叁拾玖
+ * @description: TODO
+ * @date: 2024/5/16 上午9:58
+ * @version: 1.0
  */
 public class PersonTest {
     public static void main(String[] args) {
+        //创建对象
+        Person p1 = new Person();
 
+        //通过对象调用方法、属性
+        p1.name = "拾叁拾玖";
+        p1.age = 18;
+        p1.gender = '男';
+        System.out.println("姓名： " + p1.name + "年龄：" + p1.age + "性别：" + p1.gender);
+        p1.eat();
+        p1.sleep(8);
+        p1.interests("跑步");
+        //创建第二个对象
+        Person p2 = new Person();
+        p2.name = "李师师";
+        p2.age = 18;
+        p2.gender = '女';
+        System.out.println("姓名： " + p2.name + "年龄：" + p2.age + "性别：" + p2.gender);
+        p2.eat();
+        p2.sleep(8);
+        p2.interests("唱跳");
     }
 }
